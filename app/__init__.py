82c2384f42486b91526404f9787e5a3e626835ab
Ricardo de Arruda <settiricardo@gmail.com>
2019-11-16 00:10:09 -0800

    change database config

--- app/__init__.py
+++ app/__init__.py
@@ -3,9 +3,20 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 
 
-app = Flask(__name__)
-app.config.from_object(Config)
-db = SQLAlchemy(app)
+import os
+import psycopg2
+
+DATABASE_URL = os.environ['DATABASE_URL']
+conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+
+# app = Flask(__name__)
+# app.config.from_object(Config)
+db = SQLAlchemy(conn)
 
 
 from app import routes, models
+
+
+
+
+# DATABASE_URL=postgres://{user}:{password}@{hostname}:{port}/{database-name}
--- config.py
+++ config.py
@@ -2,6 +2,9 @@ import os
 import psycopg2
 basedir = os.path.abspath(os.path.dirname(__file__))
 
+DATABASE_URL = os.environ['DATABASE_URL']
+conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+
 class Config(object):
     # ...
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \

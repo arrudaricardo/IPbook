ccaabd15516abc81511a795472a80777632ec4d0
Ricardo de Arruda <settiricardo@gmail.com>
2019-11-16 00:32:23 -0800

    change dep

--- app/__init__.py
+++ app/__init__.py
@@ -3,20 +3,9 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 
 
-import os
-import psycopg2
-
-DATABASE_URL = os.environ['DATABASE_URL']
-conn = psycopg2.connect(DATABASE_URL, sslmode='require')
-
-# app = Flask(__name__)
-# app.config.from_object(Config)
-db = SQLAlchemy(conn)
+app = Flask(__name__)
+app.config.from_object(Config)
+db = SQLAlchemy(app)
 
 
 from app import routes, models
-
-
-
-
-# DATABASE_URL=postgres://{user}:{password}@{hostname}:{port}/{database-name}
--- config.py
+++ config.py
@@ -2,11 +2,8 @@ import os
 import psycopg2
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-DATABASE_URL = os.environ['DATABASE_URL']
-conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 
 class Config(object):
     # ...
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
